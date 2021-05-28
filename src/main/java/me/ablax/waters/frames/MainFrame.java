8a37929e06e346e42aad7a7a746f3cd79be6c0c5
Murad Hamza <muradmura59@gmail.com>
2021-05-29 02:28:47 +0300

    Remove nested classes

--- src/main/java/me/ablax/waters/frames/MainFrame.java
+++ src/main/java/me/ablax/waters/frames/MainFrame.java
@@ -4,7 +4,6 @@ import me.ablax.waters.db.DBHelper;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -49,6 +48,92 @@ public class MainFrame extends JFrame {
     JButton searchBtn = new JButton("Търси");
     JComboBox<String> searchCombo = new JComboBox<>();
 
+    private final ActionListener addAction = arg0 -> {
+        conn = DBHelper.getConnection();
+        String sql = "insert into person values(null,?,?,?,?,?)";
+        try {
+            state = conn.prepareStatement(sql);
+            state.setString(1, fnameTF.getText());
+            state.setString(2, lnameTF.getText());
+            state.setString(3, sexCombo.getSelectedItem().toString());
+            state.setByte(4, Byte.parseByte(ageTF.getText()));
+            state.setFloat(5, Float.parseFloat(salaryTF.getText()));
+
+            state.execute();
+            table.setModel(DBHelper.getAllData("WATER_BODY"));
+            DBHelper.fillCombo(searchCombo);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                state.close();
+                conn.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        clearForm();
+    };
+    private final ActionListener searchAction = e -> {
+        String selectedItem = searchCombo.getSelectedItem().toString();
+        String[] items = selectedItem.split(" ");
+        int itemID = Integer.parseInt(items[0]);
+
+        conn = DBHelper.getConnection();
+        String sql = "select * from person where id=?";
+        try {
+            state = conn.prepareStatement(sql);
+            state.setInt(1, itemID);
+            result = state.executeQuery();
+            table.setModel(new GenericTable(result));
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    };
+    private final ActionListener deleteAction = e -> {
+        conn = DBHelper.getConnection();
+        String sql = "delete from person where id=?";
+        try {
+            state = conn.prepareStatement(sql);
+            state.setInt(1, id);
+            state.execute();
+            id = -1;
+            table.setModel(DBHelper.getAllData("WATER_BODY"));
+            DBHelper.fillCombo(searchCombo);
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+    };
+    private MouseListener tableListener = new MouseListener() {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            int row = table.getSelectedRow();
+            id = Integer.parseInt(table.getValueAt(row, 0).toString());
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+    };
+
     public MainFrame() {
         this.setSize(500, 600);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -80,17 +165,18 @@ public class MainFrame extends JFrame {
 
 
         this.add(midPanel);
-        addBtn.addActionListener(new AddAction());
-        deleteBtn.addActionListener(new DeleteAction());
+
+        addBtn.addActionListener(addAction);
+        deleteBtn.addActionListener(deleteAction);
         DBHelper.fillCombo(searchCombo);
-        searchBtn.addActionListener(new SearchAction());
+        searchBtn.addActionListener(searchAction);
 
         //downPanel
         this.add(downPanel);
         downPanel.add(scroller);
         scroller.setPreferredSize(new Dimension(450, 160));
         table.setModel(DBHelper.getAllData("WATER_BODY"));
-        table.addMouseListener(new TableListener());
+        table.addMouseListener(tableListener);
 
         this.setVisible(true);
 
@@ -103,123 +189,5 @@ public class MainFrame extends JFrame {
         salaryTF.setText("");
     }// end clearForm
 
-    class SearchAction implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String selectedItem = searchCombo.getSelectedItem().toString();
-            String[] items = selectedItem.split(" ");
-            int itemID = Integer.parseInt(items[0]);
-
-            conn = DBHelper.getConnection();
-            String sql = "select * from person where id=?";
-            try {
-                state = conn.prepareStatement(sql);
-                state.setInt(1, itemID);
-                result = state.executeQuery();
-                table.setModel(new GenericTable(result));
-            } catch (SQLException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            } catch (Exception e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-
-        }//end actionPerformed
-
-    }//end SearchAction
-
-    class DeleteAction implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            conn = DBHelper.getConnection();
-            String sql = "delete from person where id=?";
-            try {
-                state = conn.prepareStatement(sql);
-                state.setInt(1, id);
-                state.execute();
-                id = -1;
-                table.setModel(DBHelper.getAllData("WATER_BODY"));
-                DBHelper.fillCombo(searchCombo);
-            } catch (SQLException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-
-        }
-
-    }//end DeleteAction
-
-    class TableListener implements MouseListener {
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            int row = table.getSelectedRow();
-            id = Integer.parseInt(table.getValueAt(row, 0).toString());
-
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-    }//end TableListener
-
-    class AddAction implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent arg0) {
-            conn = DBHelper.getConnection();
-            String sql = "insert into person values(null,?,?,?,?,?)";
-            try {
-                state = conn.prepareStatement(sql);
-                state.setString(1, fnameTF.getText());
-                state.setString(2, lnameTF.getText());
-                state.setString(3, sexCombo.getSelectedItem().toString());
-                state.setByte(4, Byte.parseByte(ageTF.getText()));
-                state.setFloat(5, Float.parseFloat(salaryTF.getText()));
-
-                state.execute();
-                table.setModel(DBHelper.getAllData("WATER_BODY"));
-                DBHelper.fillCombo(searchCombo);
-            } catch (SQLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } finally {
-                try {
-                    state.close();
-                    conn.close();
-                } catch (SQLException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-
-            clearForm();
-        }
-
-    }// end class AddAction
 
 }// end class MyFrame
